773493d64ae5ea291cd926aa9d9068e8137d6c95
John R. Hicks <john@determinato.com>
2010-04-11 13:22:22 +0000

    Added Apache 2.0 License to some source files.

--- src/com/determinato/feeddroid/service/FeedDroidUpdateService.java
+++ src/com/determinato/feeddroid/service/FeedDroidUpdateService.java
@@ -1,3 +1,18 @@
+/*   
+ * Copyright 2010 John R. Hicks
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.determinato.feeddroid.service;
 
 import android.app.Activity;
@@ -94,7 +109,12 @@ public class FeedDroidUpdateService extends Service {
 		String tickerTxt = getString(R.string.updates_available);
 		String titleTxt = getString(R.string.app_name);
 		Notification notification = new Notification(icon, tickerTxt, System.currentTimeMillis());
+		notification.ledOffMS = 0;
+		notification.ledOnMS = 1;
 		notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+		notification.flags |= Notification.DEFAULT_SOUND;
+		long[] vibrate = new long[] {1000, 1000};
+		notification.vibrate = vibrate;
 		Intent appIntent = new Intent(getApplicationContext(), ChannelListActivity.class);
 		PendingIntent pending = PendingIntent.getActivity(getApplicationContext(), 0, appIntent, 0);
 		notification.setLatestEventInfo(getApplicationContext(), titleTxt, tickerTxt, pending);
