ca6af0b5deec2276b1d18dc7bbd830256f61dabd
John R. Hicks <john@determinato.com>
2010-04-14 01:10:38 +0000

    Merge from 1.0 b1 branch

--- src/com/determinato/feeddroid/activity/PreferencesActivity.java
+++ src/com/determinato/feeddroid/activity/PreferencesActivity.java
@@ -1,3 +1,18 @@
+/*   
+ * Copyright 2010 John R. Hicks
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.determinato.feeddroid.activity;
 
 
@@ -12,6 +27,7 @@ import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -151,7 +167,7 @@ public class PreferencesActivity extends Activity {
 			Log.d(TAG, "Item selected: " + id);
 		}
 		
-		public void onNothingSelected(AdapterView<?> parent) {
+		public void onNothingSelected(AdapterView parent) {
 			
 		}
 	}
